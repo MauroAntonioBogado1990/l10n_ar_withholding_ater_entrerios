e0e3c1547f2c6d3fa2177eb91a244bb4dee24353
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2026-03-19 13:50:40 -0300

    fix in depends

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Argentina - Retenciones y Percepciones ATER Entre Ríos',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Retenciones y Percepciones de IIBB para la provincia de Entre Ríos (ATER)',
     'description': """
         Módulo para la gestión de retenciones y percepciones de Ingresos Brutos
@@ -12,18 +12,20 @@
         - Sujetos pasivos: inscriptos en IIBB con jurisdicción en Entre Ríos
           (contribuyentes directos y Convenio Multilateral)
     """,
-    'author': 'Greenworking',
+    'author': 'Mauro Bogado, Exemax',
     'category': 'Accounting/Localizations',
     'depends': [
         'account',
         'l10n_ar',
-        'l10n_ar_partner',
-        'sale',
-        'l10n_ar_withholding_automatic',
+        #'l10n_ar_partner',
+        'contacts',
+        'sale_management',
+        #'l10n_ar_withholding_automatic',
+        #'l10n_ar_withholding',
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/account_tax_inherit_view.xml',
+        #'views/account_tax_inherit_view.xml',
         'views/res_company_view.xml',
         'views/res_partner_view.xml',
         'views/account_export_ater_entrerios_view.xml',
